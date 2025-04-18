5caf6b93999755b57ffa8ddde9b62321f8f176ba
andylarsen123 <andyr.larsen@gmail.com>
2025-04-17 21:55:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,29 @@ document.getElementById('dark-toggle').addEventListener('click', () => {
 
 const cursorDot = document.querySelector('.cursor-dot');
 
-document.addEventListener('mousemove', (e) => {
-  cursorDot.style.left = `${e.clientX}px`;
-  cursorDot.style.top = `${e.clientY}px`;
-});
+if (cursorDot) {
+  let mouseX = 0;
+  let mouseY = 0;
+  let dotX = 0;
+  let dotY = 0;
+
+  document.addEventListener('mousemove', (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+  });
+
+  function animateDot() {
+    dotX += (mouseX - dotX) * 0.15;
+    dotY += (mouseY - dotY) * 0.15;
+
+    cursorDot.style.left = `${dotX}px`;
+    cursorDot.style.top = `${dotY}px`;
+
+    requestAnimationFrame(animateDot);
+  }
+
+  animateDot();
+} else {
+  console.warn('Cursor dot element not found in the DOM.');
+}
+
